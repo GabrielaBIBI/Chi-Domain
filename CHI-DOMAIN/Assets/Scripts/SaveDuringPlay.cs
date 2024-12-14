d391fa29be63f34106f93e5eb9367ff09816943f
GabrielaBIBI <gabirisrodrigues@gmail.com>
2024-12-13 21:18:58 -0300

    13-12-24
    cinemachine atualizado

---
+++ CHI-DOMAIN/Assets/Scripts/SaveDuringPlay.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+
+/*
+
+namespace Unity.Cinemachine {
+    public sealed class SaveDuringPlayAttribute : System.Attribute {}
+    public sealed class NoSaveDuringPlayAttribute : PropertyAttribute {}
+}
+
+using Unity.Cinemachine;
+
+[SaveDuringPlay]
+public class SaveDuringPlay : MonoBehaviour
+{
+    public float speed = 10f;
+    [NoSaveDuringPlay] public float maxSpeed = 20f;
+    public Vector3 trackedOffset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+*/
