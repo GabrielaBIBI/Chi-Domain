b89b8bd8768a9aaaed4cb04feaa45b68a3c85233
GabrielaBIBI <gabirisrodrigues@gmail.com>
2025-02-07 17:56:20 -0300

    swordmovement

--- CHI-DOMAIN/Assets/Scripts/SwordMovement.cs
+++ CHI-DOMAIN/Assets/Scripts/SwordMovement.cs
@@ -2,22 +2,37 @@ using UnityEngine;
 
 public class SwordMovement : MonoBehaviour
 {
+    public float AngleSword = 45f;
     public SpriteRenderer spriteRender;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
+        //Flip
         float x = Input.GetAxis("Horizontal");
 
-        //Flip
         if (x != 0 && x < 0){
             spriteRender.flipX = true;
+            AngleSword = -45f;
         } else if (x != 0 && x > 0){
             spriteRender.flipX = false;
+            AngleSword = 45f;          
         }
+        
+
+
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            transform.rotation = Quaternion.Euler(0,0, AngleSword);
+        }
+        
     }
+  
+
+
 }
+
